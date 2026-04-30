35d7255cc133bec8795b543972f90b65f7aeeb11
Chester Tejido <chamtejido@gmail.com>
2026-04-30 18:40:04 -0400

    Added test element and a 3d object to canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Header } from './header';
+import { useRef } from 'react';
+import * as THREE from 'three';
 
-export const App: React.FC = () => {
+function Box() {
+	const meshRef = useRef<THREE.Mesh>(null);
+
+	// Rotate box mesh
+	useFrame((_, delta) => {
+		if (meshRef.current) {
+			meshRef.current.rotation.y += delta * 0.6;
+		}
+	});
+
 	return (
-		<div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-			<h1>React Test Page</h1>
-			<p>Welcome to your test application.</p>
-			<button onClick={() => alert('Button clicked!')}>Click Me</button>
+		<mesh ref={meshRef} rotation={[10, 10, 0]}>
+			<boxGeometry args={[1, 1, 1]} />
+			<meshStandardMaterial color="white " />
+		</mesh>
+	);
+}
+
+export function App() {
+	return (
+		<div style={{ width: '100vw', height: '100vh' }}>
+			<Header />
+			<Canvas frameloop="always">
+				<hemisphereLight groundColor="orange" intensity={0.35} />
+				<directionalLight position={[5, 5, 5]} intensity={1} castShadow />
+				<ambientLight intensity={0.25} />
+				<pointLight position={[-10, 10, -10]} intensity={0.5} />
+				<Box />
+			</Canvas>
 		</div>
 	);
-};
+}
 
 export default App;
