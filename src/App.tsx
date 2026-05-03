9aae46fb5e63d1efd7858ff3fc41af4a02c6d04e
Chester Tejido <47069727+chester-tejido@users.noreply.github.com>
2026-05-02 20:42:54 -0400

    React fiber debugger (#27)
    * Added leva library as GUI
    
    * Added camera controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { Header } from './header';
 import { TestCube } from './test-cube';
 import { TestModel } from './test-model';
+import * as THREE from 'three';
+import { CameraControls, CameraControlsImpl } from '@react-three/drei';
+import { Ref, RefObject, useRef, useState, type MutableRefObject } from 'react';
+import { folder, useControls } from 'leva';
+import { CameraStats } from './camera-stats';
 
 export function App() {
+	const { ACTION } = CameraControlsImpl;
+	const cameraControlsRef = useRef<CameraControlsImpl>(null);
+
 	return (
-		<div style={{ width: '100vw', height: '100vh' }}>
+		<div style={{ width: '100vw', height: '100vh', position: 'relative' }}>
 			<Header />
-			<Canvas shadows frameloop="always" camera={{ position: [5, 5, 5], fov: 50 }}>
+			<Canvas
+				shadows={{
+					enabled: true,
+					type: THREE.PCFShadowMap,
+				}}
+				frameloop="always"
+				camera={{ position: [5, 5, 5], fov: 50 }}
+			>
+				<CameraControls
+					mouseButtons={{
+						left: ACTION.ROTATE,
+						middle: ACTION.TRUCK,
+						right: ACTION.DOLLY,
+						wheel: ACTION.DOLLY,
+					}}
+					ref={cameraControlsRef}
+				/>
+				<CameraStats cameraControlsRef={cameraControlsRef} />
 				<hemisphereLight groundColor="white" intensity={0.35} />
 				<directionalLight position={[10, 10, 10]} intensity={1} />
 				<directionalLight position={[0, 10, 0]} intensity={0.5} castShadow />
