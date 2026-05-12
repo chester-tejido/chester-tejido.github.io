1391f1becdcfd3d9cf038f0b60502847f54d8db8
Chester Tejido <47069727+chester-tejido@users.noreply.github.com>
2026-05-12 16:44:48 -0400

    Added dev and prod environment (#38)

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import { CameraControls, CameraControlsImpl } from '@react-three/drei';
 import { useRef } from 'react';
 import { CameraStats } from './camera-stats';
 import { PerspectiveRoom } from './perspective-room';
+import { Leva } from 'leva';
 
 export function App() {
 	const { ACTION } = CameraControlsImpl;
 	const cameraControlsRef = useRef<CameraControlsImpl>(null);
+	const isProduction = process.env.NODE_ENV === 'production';
 
 	return (
 		<div style={{ width: '100vw', height: '100vh', position: 'relative' }}>
 			<Header />
+			<Leva hidden={isProduction} />
 			<Canvas
 				shadows={{
 					enabled: true,
@@ -21,16 +24,21 @@ export function App() {
 				frameloop="always"
 				camera={{ position: [12, 12, 12], fov: 50 }}
 			>
-				<CameraControls
-					mouseButtons={{
-						left: ACTION.ROTATE,
-						middle: ACTION.TRUCK,
-						right: ACTION.DOLLY,
-						wheel: ACTION.DOLLY,
-					}}
-					ref={cameraControlsRef}
-				/>
-				<CameraStats cameraControlsRef={cameraControlsRef} />
+				{!isProduction && (
+					<>
+						<CameraControls
+							mouseButtons={{
+								left: ACTION.ROTATE,
+								middle: ACTION.TRUCK,
+								right: ACTION.DOLLY,
+								wheel: ACTION.DOLLY,
+							}}
+							ref={cameraControlsRef}
+						/>
+						<CameraStats cameraControlsRef={cameraControlsRef} />
+					</>
+				)}
+
 				<hemisphereLight groundColor="white" intensity={0.35} />
 				<directionalLight position={[10, 10, 10]} intensity={1} />
 				<directionalLight position={[0, 10, 0]} intensity={0.5} castShadow />
